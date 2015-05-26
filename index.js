a0002e2132b4f3d9192621c42814531b763d0014
Stephan Wallentin <stephan.wallentin@gmail.com>
2015-05-27 00:16:29 +0200

    Add kue support for rendering and transcoding quake live demos. Improved execution flow using async lib. Minor refactoring to improve readability.

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ var ps = require('ps-node'),
 	os = require('os'),
 	exec = require('child_process').exec,
 	util = require('util'),
+	async = require('async'),
 	getMostRecentFilenameFromFolder = require('./getMostRecentFilenameFromFolder'),
 	settings = require('./settings'),
-	demoFile = 'inl(POV)-vs-JoXmeister-sinister-2015_05_24-23_45_45.dm_90';
+  kue = require('kue'),
+  queue = kue.createQueue({
+    port: 1234,
+    host: 'home.codejutsu.se'
+  });
+
+kue.app.listen(3000);
 
 function isProcessRunning(options, cb) {
 	ps.lookup(options, function(err, resultList ) {
@@ -12,7 +19,7 @@ function isProcessRunning(options, cb) {
 			throw new Error( err );
 		}
 
-		console.log(err,resultList, options);
+		//console.log(err,resultList, options);
 
 		cb(resultList.length > 0);
 	});
@@ -32,45 +39,85 @@ function isTranscodingVideo(cb) {
 	}, cb);
 }
 
+function render(demoFilename, callback) {
+	isRenderingDemo(function(isRendering) {
 
+		if (isRendering) {
+			return callback(new Error('Already busy performing rendering.'));
+		}
 
+    // TODO: Check if file exists on disc
 
+		console.log('started rendering of: ', demoFilename);
+		var renderDemoCommand = util.format(settings.renderDemoCommandFormat, settings.WOLFCAM_HOME, settings.WOLFCAM_CMD, settings.WOLFCAM_HOME, demoFilename);
+		console.log('Executing:', renderDemoCommand);
 
-isRenderingDemo(function(isRendering) {
-
-	if (isRendering) {
-			return console.log('Already busy performing rendering.');
-	}
-
-	console.log('started rendering of: ', demoFile);
-	var renderDemoCommand = util.format(settings.renderDemoCommandFormat, settings.wolfcam_home, settings.wolfcam_executable, settings.wolfcam_home,  demoFile);
-	console.log(renderDemoCommand);
-
-	exec(renderDemoCommand, function (a, b, c) {
-		isTranscodingVideo(function (isTranscoding) {
-
-			if(isTranscoding) {
-				return console.log("Already busy transcoding");
-			}
-			console.log('start transcoding of:', demoFile);
-			var demoOutputFile = settings.videosFolder + getMostRecentFilenameFromFolder(settings.videosFolder),
-				transcodeDemoCommand = util.format(settings.transcodeVideoCommandFormat, demoOutputFile, settings.videosFolder + demoFile);
-
-			console.log(transcodeDemoCommand);
-
-
-			exec(transcodeDemoCommand, function (a, b, c) {
-				console.log('Finished handbrake');
-				exec('rm -rf ' + demoOutputFile, function () {
-					console.log('Removed outputfile', demoOutputFile);
-				});
-			});
-
+		exec(renderDemoCommand, function (a, b, c) {
+      var renderOutputPath = util.format("%s/%s", settings.videosFolder, getMostRecentFilenameFromFolder(settings.videosFolder));
+      console.log(renderOutputPath);
+      console.log(demoFilename);
+      callback(null, demoFilename, renderOutputPath);
 		});
+
 	});
 
+}
+
+function transcode(demoFilename, renderOutputPath, callback) {
+	isTranscodingVideo(function (isTranscoding) {
+		if(isTranscoding) {
+			return callback(new Error("Already busy transcoding"));
+		}
+
+		console.log('start transcoding of:', demoFilename);
+    console.log(renderOutputPath);
+
+      var transcodeOutputFullPath = util.format(settings.transcodeVideoOutputFullpathFormat, settings.videosFolder, demoFilename);
+			transcodeDemoCommand = util.format(settings.transcodeVideoCommandFormat, renderOutputPath, transcodeOutputFullPath);
+
+    console.log(transcodeOutputFullPath);
+    console.log('Exec:', transcodeDemoCommand);
+
+		exec(transcodeDemoCommand, function (a, b, c) {
+			console.log('Finished handbrake');
+			callback(null, demoFilename, renderOutputPath, transcodeOutputFullPath);
+		});
+	});
+}
+
+function removeRenderOutputFile(demoFilename, renderOutputPath, transcodeOutputFullPath, callback) {
+		exec('rm -rf ' + renderOutputPath, function () {
+			console.log('Removed outputfile', renderOutputPath);
+			callback(null, transcodeOutputFullPath);
+		});
+}
+
+queue.process('render-demo', function (job, done) {
+
+  function init(callback) {
+    callback(null, job.data.demofile);
+  }
+
+  async.waterfall([
+    init,
+    render,
+    transcode,
+    removeRenderOutputFile], function(err, result) {
+    console.log('Rendering is finished. See result: ', result);
+    console.log('Job done');
+    done && done();
+  });
 
 });
+
+var job = queue.create('render-demo', {
+  demofile: 'inl(POV)-vs-JoXmeister-sinister-2015_05_24-23_45_45.dm_90'
+}).attempts(1000).backoff({ delay: 3000, type: 'fixed' }).save(function (err) {
+  if (!err) {
+    console.log(job.id);
+  }
+});
+
 
 
 // handbroke renderDemoCommand to render output
--- kue.js
+++ kue.js
@@ -7,16 +7,16 @@ var kue = require('kue'),
 kue.app.listen(3000);
 
 var job = queue.create('render-demo', {
-  source: ''
+  demofile: 'inl(POV)-vs-JoXmeister-sinister-2015_05_24-23_45_45.dm_90'
 }).attempts(1000).backoff({ delay: 3000, type: 'fixed' }).save(function (err) {
   if (!err) {
     console.log(job.id);
   }
 });
 
-queue.process('render-demo', function (job, done) {
-  var err = new Error('purposely fail job');
-  job.failed().error(err);
-  done && done(err);
-  console.log('Failed the job');
-});
+//queue.process('render-demo', function (job, done) {
+//  var err = new Error('purposely fail job');
+//  job.failed().error(err);
+//  done && done(err);
+//  console.log('Failed the job');
+//});
